d2ce14fc2f4681758c9147622cda527dc8b92c7a
Busakuza <kazadan@post.bgu.ac.il>
2022-09-08 17:59:20 +0300

    Add project files.

---
+++ ProcessManagmentApp/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace ProcessManagmentApp
+{
+    partial class ProcessManager
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("Name", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup2 = new System.Windows.Forms.ListViewGroup("Id", System.Windows.Forms.HorizontalAlignment.Left);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProcessManager));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.NameColumn = new System.Windows.Forms.ColumnHeader("(none)");
+            this.IdColumn = new System.Windows.Forms.ColumnHeader();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(353, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Terminate";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.White;
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.NameColumn,
+            this.IdColumn});
+            this.listView1.ForeColor = System.Drawing.Color.Black;
+            listViewGroup1.Header = "Name";
+            listViewGroup1.Name = "Name";
+            listViewGroup2.Header = "Id";
+            listViewGroup2.Name = "Id";
+            this.listView1.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup1,
+            listViewGroup2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(151, 58);
+            this.listView1.Name = "listView1";
+            this.listView1.ShowGroups = false;
+            this.listView1.Size = new System.Drawing.Size(196, 367);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // NameColumn
+            // 
+            this.NameColumn.Text = "Name";
+            this.NameColumn.Width = 80;
+            // 
+            // IdColumn
+            // 
+            this.IdColumn.Text = "Id";
+            this.IdColumn.Width = 80;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(104, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(243, 33);
+            this.panel1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(47, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Enter Process Name/Id";
+            this.textBox1.Size = new System.Drawing.Size(196, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(41, 32);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(197, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(209, 33);
+            this.panel2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(365, 405);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(418, 405);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Processes";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ProcessManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(549, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(567, 497);
+            this.MinimumSize = new System.Drawing.Size(567, 497);
+            this.Name = "ProcessManager";
+            this.Text = "Process Manager";
+            this.Load += new System.EventHandler(this.ProcessManager_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader NameColumn;
+        private System.Windows.Forms.ColumnHeader IdColumn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ProcessManagmentApp/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessManagmentApp
+{
+    public partial class ProcessManager : Form
+    {
+        private Dictionary<string, string> processes = new Dictionary<string, string>();// the key is the process id and the value is it's name.
+        private int toSort;
+
+        /*
+         ListViewColumnSorter is a private class which can only be called from ProcessManager, and it's implementing the IComparer
+         iterface which overrides the compare method for the Sorting of the items, by the process name or by the process id 
+         */
+        private class ListViewColumnSorter : IComparer
+        {
+            /// Specifies the column to be sorted
+            public int ColumnToSort { get; set; }
+
+            /// Specifies the order in which to sort (i.e. 'Ascending').
+            public SortOrder OrderOfSort { get; set; }
+
+            public ListViewColumnSorter(int columnIndex)
+            {
+                ColumnToSort = columnIndex;
+                OrderOfSort = SortOrder.None;
+            }
+
+            
+            public int Compare(object x, object y)
+            {
+                int compareResult=0;
+                int left, right;
+                ListViewItem listviewX, listviewY;
+
+                // Cast the objects to be compared to ListViewItem objects
+                listviewX = (ListViewItem)x;
+                listviewY = (ListViewItem)y;
+
+                // compare the 2 items depending on the selected column
+                switch (ColumnToSort)
+                {
+                    case 0:
+                        compareResult = string.Compare(listviewX.Name, listviewY.Name);
+                        break;
+                    case 1:
+                        left = Int32.Parse(listviewX.SubItems[ColumnToSort].Text);
+                        right = Int32.Parse(listviewY.SubItems[ColumnToSort].Text);
+                        compareResult = left > right ? 1 : -1;
+                        break;
+                }
+
+                if (OrderOfSort == SortOrder.Ascending)
+                {
+                    return compareResult;
+                }
+                else if (OrderOfSort == SortOrder.Descending)
+                {
+                    return (-compareResult);
+                }
+                return 0;
+                
+            }
+
+
+        }
+        public ProcessManager()
+        {;
+            InitializeComponent();
+            showProcessess();
+            toSort = 0;
+        }
+        /*
+         showProcesses - refreshes the listview items whenever we kill some of the processess and adds the current running 
+                         processess to the list.
+         */
+        private void showProcessess()
+        {
+            listView1.Items.Clear();
+            Process[] s = Process.GetProcesses();
+            
+            foreach (Process process in s)
+            {
+                ListViewItem item = new ListViewItem(process.ProcessName);
+                item.Name = process.ProcessName;
+                item.SubItems.Add(process.Id.ToString());
+                item.Tag = process;
+                listView1.Items.Add(item);
+                if(!processes.ContainsKey(process.Id.ToString()))
+                    processes.Add(process.Id.ToString(), process.ProcessName);
+
+
+            }
+            label1.Text = listView1.Items.Count.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //check if text was entered to the textbox and see if it's in the processess' list or in the dictionary of the processess and mark the
+            //relevant item as selected
+            string processName = textBox1.Text;
+            if (listView1.Items.ContainsKey(processName))
+                listView1.Items[listView1.Items.IndexOfKey(processName)].Selected = true;
+
+            else if (processes.ContainsKey(processName))
+                listView1.Items[listView1.Items.IndexOfKey(processes[processName])].Selected = true;
+
+            listView1.Select();
+            //pop up message box if the process doesn't exist
+            if(listView1.SelectedItems.Count==0)
+                MessageBox.Show("Process "+processName+ " Not Running", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            foreach (ListViewItem process in listView1.SelectedItems)
+            {
+                try
+                {
+                    //try killing the processess from the selected items. exception is thrown if we don't have permission for a specific process
+                    ((Process)(process.Tag)).Kill();
+                    processes.Remove(((Process)(process.Tag)).Id.ToString());
+                    MessageBox.Show("Process " + ((Process)(process.Tag)).ProcessName+" Is Terminated", "Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show("Can't Kill Process " + ((Process)(process.Tag)).ProcessName, exception.Message, MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+            }
+               
+               
+            showProcessess();
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            ListViewColumnSorter listViewColumnSorter = new ListViewColumnSorter(e.Column);
+            listView1.ListViewItemSorter = listViewColumnSorter;
+            switch (toSort)
+            {
+                case 0:
+                    listViewColumnSorter.OrderOfSort = SortOrder.Ascending;
+                    listView1.Sort();
+                    toSort++;
+                    break;
+                case 1:
+                    listViewColumnSorter.OrderOfSort = SortOrder.Descending;
+                    listView1.Sort();
+                    toSort--;
+                    break;
+            }
+            
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            showProcessess();
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ProcessManager_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            showProcessess();
+        }
+    }
+}
